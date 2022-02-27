8fff39004a6a65000860020ff935ff8a262a1f07
Josh Marroquin <mj.marroquin993@gmail.com>
2022-02-27 15:34:56 -0800

    commit tree 8b7e4290cde19b23d8b81589b4be72e61c5bc437

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Write your solution in this file!
+var customerName = 'bob'
+function names () {
+    return customerName;
+}
+
+function upperCaseCustomerName () {
+    customerName = 'BOB';
+}
+
+var bestCustomer = undefined
+function setBestCustomer () {
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer () {
+    bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'name'
+function changeLeastFavoriteCustomer () {
+    leastFavoriteCustomer = 'marisa'
+}
